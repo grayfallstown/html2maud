d9530e942f502ef119961bb2557869597a064cd1
grayfallstown <grayfallstown@protonmail.com>
2023-01-03 09:06:01 +0100

    initial commit

---
+++ binary/src/main.rs
@@ -0,0 +1,54 @@
+use html2maud::*;
+use clap::{Parser};
+use std::io::{self, BufRead};
+use std::fs;
+
+#[derive(Parser, Debug)]
+struct Args {
+    #[arg(short = 'i', long="in", value_name = "input html file", value_hint = clap::ValueHint::FilePath)]
+    r#in: Option<std::path::PathBuf>,
+
+    #[arg(short = 'o', long="out", value_name = "output maud file", value_hint = clap::ValueHint::FilePath)]
+    out: Option<std::path::PathBuf>,
+    
+    #[arg(short = 's', long="stdin", value_name = "read from stdin")]
+    stdin: bool,
+}
+
+
+fn run_cli(args: &Args) {
+    let html = if args.stdin {
+        let stdin = io::stdin();
+        let stdin_string: String = stdin.lock().lines()
+            .map(|line| line.expect("could not read from stdin"))
+            .collect::<Vec<String>>().join("\n");
+        stdin_string
+    } else {
+        match &args.r#in {
+            None => panic!("input file must be Some(file) here"),
+            Some(file) => std::fs::read_to_string(file).expect("could not read input file")
+        }
+    };
+
+    let maudtemplate = html2maud(&html);
+
+    match &args.out {
+        None => println!("{}", maudtemplate),
+        Some(file) => fs::write(file, maudtemplate).expect("Unable to write output file"),
+    }
+}
+
+fn run_gui() {
+    println!("neither --stdin nor --in specified. Starting gui. If you wanted to use the cli, use html2maaud --help");
+    panic!("gui not yet implemented");
+}
+
+fn main() {
+    let args = Args::parse();
+
+    if !args.stdin && args.r#in.is_none() {
+        run_gui();
+    } else {
+        run_cli(&args);
+    }
+}
---
+++ library/src/lib.rs
@@ -0,0 +1,96 @@
+
+use std::fmt::Write;
+use tl::*;
+
+pub fn html2maud(html: &str) -> String {
+    let mut maud_template = String::new();
+
+
+    let dom = tl::parse(html, tl::ParserOptions::default()).unwrap();
+    let parser = dom.parser();
+
+    fn spaces(count: usize) -> String {
+        return "\t".repeat(count).as_str().to_owned();
+    }
+
+    fn handle_tag(tag: &HTMLTag, parser: &Parser, maud_template: &mut String, indent: usize) {
+        let tag_name = tag.name().as_utf8_str();
+        write!(maud_template, "{}{}", spaces(indent), &tag_name).unwrap();
+        let id = tag.attributes().id().map(|x| x.as_utf8_str());
+        match &id {
+            Option::Some(x) => {
+                let escaped_id = if x.contains("-") {
+                    format!("\"{}\"", &x)
+                } else {
+                    x.to_string()
+                };
+                write!(maud_template, " #{}", &escaped_id).unwrap();
+            },
+            Option::None => {},
+        }
+
+        match tag.attributes().class_iter() {
+            None => {},
+            Some(classes) => {
+                for class in classes {
+                    let escaped_class = if class.contains("-") {
+                        format!("\"{}\"", &class)
+                    } else {
+                        class.to_owned()
+                    };
+                    
+                    write!(maud_template, ".{}", &escaped_class).unwrap();
+                }
+            }
+        }
+        
+        for (key, value_opt) in tag.attributes().iter() {
+            if !(key.eq("id") || key.eq("class")) {
+                write!(maud_template, " {}", key).unwrap();
+                match value_opt {
+                    None => {},
+                    Some(value) => write!(maud_template, "\"{}\"", value).unwrap(),
+                }    
+            }
+        }
+
+        write!(maud_template, " {{\n").unwrap();
+        let children = tag.children();
+        let nodes = children.top().as_slice();
+        for child_node in nodes {
+            handle_node(child_node.get(parser), parser, maud_template, indent + 1);
+            write!(maud_template, "\n").unwrap();
+        }
+        write!(maud_template, "{}}}", spaces(indent)).unwrap();
+    }
+
+    fn handle_node(node_opt: Option<&Node>, parser: &Parser, maud_template: &mut String, indent: usize) {
+        match node_opt {
+            None => {},
+            Some(node) => {
+                match node {
+                    Node::Tag(tag) => handle_tag(tag, parser, maud_template, indent),
+                    Node::Comment(_) => {},
+                    Node::Raw(raw) => {
+                        let text = raw.as_utf8_str();
+                        let trimmed_text = text.trim();
+                        if !trimmed_text.is_empty() {
+                            write!(maud_template, "{}\"{}\"", spaces(indent), trimmed_text.replace("\"","\\\"")).unwrap();
+                        }
+                    }
+                }
+
+     
+            }
+        }
+    }
+
+    for node_handle in dom.children() {
+        handle_node(node_handle.get(parser), parser, &mut maud_template, 0);
+    }
+    
+
+    //println!("{}", maud_template);
+    maud_template
+
+}
