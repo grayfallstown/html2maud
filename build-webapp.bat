f6246d702bd9e294823f59e0fb6c8d4baae66579
grayfallstown <grayfallstown@protonmail.com>
2023-10-21 13:17:09 +0200

    fixed github pages path

--- build-webapp.bat
+++ build-webapp.bat
@@ -1,6 +1,6 @@
 rm index* trunk*
 cd yew
 echo "github does not allow usage of github-pages folder. Must be docs or root"
-trunk build --release -d ../docs
+trunk build --release --public-url "/html2maud/" -d ../docs
 cd ..
 git status
