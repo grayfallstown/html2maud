9eeb853d0885ab042f67493683535c6eec949d71
Massimo Fraschetti <subscriptions@fraschetti.me>
2023-11-05 09:48:39 -0500

    updated

--- library/src/lib.rs
+++ library/src/lib.rs
@@ -1,4 +1,3 @@
-
 use std::fmt::Write;
 use tl::*;
 
@@ -11,7 +10,6 @@ fn remove_empty_lines(s: &str) -> String {
 pub fn html2maud(html: &str) -> String {
     let mut maud_template = String::new();
 
-
     let dom = tl::parse(html, tl::ParserOptions::default()).unwrap();
     let parser = dom.parser();
 
@@ -22,21 +20,22 @@ pub fn html2maud(html: &str) -> String {
     fn handle_tag(tag: &HTMLTag, parser: &Parser, maud_template: &mut String, indent: usize) {
         let tag_name = tag.name().as_utf8_str();
         write!(maud_template, "{}{}", spaces(indent), &tag_name).unwrap();
-        let id = tag.attributes().id().map(|x| x.as_utf8_str());
-        match &id {
-            Option::Some(x) => {
-                let escaped_id = if x.contains("-") {
-                    format!("\"{}\"", &x)
-                } else {
-                    x.to_string()
-                };
-                write!(maud_template, " #{}", &escaped_id).unwrap();
-            },
-            Option::None => {},
-        }
+        // let id = tag.attributes().id().map(|x| x.as_utf8_str());
+        // match &id {
+        //     Option::Some(x) => {
+        //         let escaped_id = if x.contains("-") {
+        //             format!("\"{}\"", &x)
+        //         } else {
+        //             x.to_string()
+        //         };
+        //         write!(maud_template, " #{}", &escaped_id).unwrap();
+        //     },
+        //     Option::None => {},
+        // }
 
+        // FIX THIS
         match tag.attributes().class_iter() {
-            None => {},
+            None => {}
             Some(classes) => {
                 for class in classes {
                     let escaped_class = if class.contains("-") {
@@ -44,19 +43,18 @@ pub fn html2maud(html: &str) -> String {
                     } else {
                         class.to_owned()
                     };
-                    
                     write!(maud_template, ".{}", &escaped_class).unwrap();
                 }
             }
         }
-        
+
         for (key, value_opt) in tag.attributes().iter() {
             if !(key.eq("id") || key.eq("class")) {
                 write!(maud_template, " {}", key).unwrap();
                 match value_opt {
-                    None => {},
+                    None => {}
                     Some(value) => write!(maud_template, "=\"{}\"", value).unwrap(),
-                }    
+                }
             }
         }
 
@@ -75,22 +73,31 @@ pub fn html2maud(html: &str) -> String {
         write!(maud_template, "{}}}\n", spaces(indent)).unwrap();
     }
 
-    fn handle_node(node_opt: Option<&Node>, parser: &Parser, maud_template: &mut String, indent: usize) {
+    fn handle_node(
+        node_opt: Option<&Node>,
+        parser: &Parser,
+        maud_template: &mut String,
+        indent: usize,
+    ) {
         match node_opt {
-            None => {},
-            Some(node) => {
-                match node {
-                    Node::Tag(tag) => handle_tag(tag, parser, maud_template, indent),
-                    Node::Comment(_) => {},
-                    Node::Raw(raw) => {
-                        let text = raw.as_utf8_str();
-                        let trimmed_text = text.trim();
-                        if !trimmed_text.is_empty() {
-                            write!(maud_template, "{}\"{}\"\n", spaces(indent), trimmed_text.replace("\"","\\\"")).unwrap();
-                        }
+            None => {}
+            Some(node) => match node {
+                Node::Tag(tag) => handle_tag(tag, parser, maud_template, indent),
+                Node::Comment(_) => {}
+                Node::Raw(raw) => {
+                    let text = raw.as_utf8_str();
+                    let trimmed_text = text.trim();
+                    if !trimmed_text.is_empty() {
+                        write!(
+                            maud_template,
+                            "{}\"{}\"\n",
+                            spaces(indent),
+                            trimmed_text.replace("\"", "\\\"")
+                        )
+                        .unwrap();
                     }
                 }
-            }
+            },
         }
     }
 
@@ -99,7 +106,6 @@ pub fn html2maud(html: &str) -> String {
         handle_node(node_handle.get(parser), parser, &mut maud_template, 1);
     }
     write!(maud_template, "\n}}\n").unwrap();
-    
-    remove_empty_lines(&maud_template)
 
+    remove_empty_lines(&maud_template)
 }
